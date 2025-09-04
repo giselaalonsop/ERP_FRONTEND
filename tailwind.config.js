647592052298ea1e753f0ace600ff089dd3363b4
giselaalonsop <giselaalonsop@gmail.com>
2025-09-03 22:34:43 -0400

    4e

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,20 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite/**/*.js",
-    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      fontSize: { base: '18px', sm: '16px', lg: '20px', xl: '24px', '2xl': '30px' },
+      fontSize: {
+        'base': '18px',
+        'sm': '16px',
+        'lg': '20px',
+        'xl': '24px',
+        '2xl': '30px',
+    },
     },
   },
-  plugins: [require('flowbite/plugin')],
-}
+  plugins: [],
+};
